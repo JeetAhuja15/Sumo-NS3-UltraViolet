5f5e35d5bd52074957c25c58e245558825cbf54a
Jeet Ahuja <91070824+JeetAhuja15@users.noreply.github.com>
2022-09-08 03:19:52 +0530

    Throughput and Latency printed...

--- sumo_ns3/basic_lte.cc
+++ sumo_ns3/basic_lte.cc
@@ -3,23 +3,63 @@
 #include "ns3/internet-module.h"
 #include "ns3/mobility-module.h"
 #include "ns3/lte-module.h"
+#include "ns3/lte-module.h"
+#include "ns3/flow-monitor-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/point-to-point-module.h"
 #include "ns3/config-store-module.h"
 #include "ns3/netanim-module.h"
+#include "ns3/gnuplot.h"
 #include <math.h>
 #include <iostream>
+#include <chrono>
 
 using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("lte_basic");
+
+void ThroughputMonitor (FlowMonitorHelper *fmhelper, Ptr<FlowMonitor> flowMon,Gnuplot2dDataset DataSet)
+  {
+    double localThrou=0;
+    std::map<FlowId, FlowMonitor::FlowStats> flowStats = flowMon->GetFlowStats();
+    Ptr<Ipv4FlowClassifier> classing = DynamicCast<Ipv4FlowClassifier> (fmhelper->GetClassifier());
+    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator stats = flowStats.begin (); stats != flowStats.end (); ++stats)
+    {
+      Ipv4FlowClassifier::FiveTuple fiveTuple = classing->FindFlow (stats->first);
+       if (fiveTuple.sourceAddress == Ipv4Address("1.0.0.2"))
+      {
+
+      std::cout<<"Flow ID     : " << stats->first <<" ; "<< fiveTuple.sourceAddress <<" -----> "<<fiveTuple.destinationAddress<<std::endl;
+      std::cout<<"Tx Packets : " << stats->second.txPackets<<std::endl;
+      std::cout<<"Rx Packets : " << stats->second.rxPackets<<std::endl;
+      std::cout<<"Duration    : "<<(stats->second.timeLastRxPacket.GetSeconds()-stats->second.timeFirstTxPacket.GetSeconds())<<std::endl;
+      std::cout<<"Last Received Packet  : "<< stats->second.timeLastRxPacket.GetSeconds()<<" Seconds"<<std::endl;
+      std::cout<<"Throughput: " << stats->second.rxBytes * 8.0 / (stats->second.timeLastRxPacket.GetSeconds()-stats->second.timeFirstTxPacket.GetSeconds())/1024  << " Kbps"<<std::endl;
+      std::cout<< "Mean{Delay}: " << (stats->second.delaySum.GetSeconds()/stats->second.rxPackets) << "\n";
+      std::cout<< "Mean{Jitter}: " << (stats->second.jitterSum.GetSeconds()/(stats->second.rxPackets)) << "\n";
+      std::cout<< "Total{Delay}: " << (stats->second.delaySum.GetSeconds()) << "\n";
+      std::cout<< "Total{Jitter}: " << (stats->second.jitterSum.GetSeconds()) << "\n";
+      std::cout<< "Lost Packets: " << (stats->second.lostPackets) << "\n";
+      std::cout<< "Dropped Packets: " << (stats->second.packetsDropped.size()) << "\n";
+      localThrou=(stats->second.rxBytes * 8.0 / (stats->second.timeLastRxPacket.GetSeconds()-stats->second.timeFirstTxPacket.GetSeconds())/1024);
+      // updata gnuplot data
+            DataSet.Add((double)Simulator::Now().GetSeconds(),(double) localThrou);
+      std::cout<<"---------------------------------------------------------------------------"<<std::endl;
+      }
+    }
+      Simulator::Schedule(Seconds(1 ),&ThroughputMonitor, fmhelper, flowMon,DataSet);
+      {
+  flowMon->SerializeToXmlFile ("ThroughputMonitor.xml", true, true);
+      }
+
+  }
 
 int main (int argc, char *argv[])
 {
 
   uint16_t numofUes = 1;
   uint16_t numofEnbs = 2;
-  Time simTime = MilliSeconds (1000);
+  Time simTime = MilliSeconds (10000);
 
   // To use EPC with LTE  
   Ptr<LteHelper> lteHelper = CreateObject<LteHelper> ();
@@ -143,6 +183,49 @@ int main (int argc, char *argv[])
   Simulator::Stop (simTime);
   AnimationInterface anim ("my_lte_basic.xml");
   //anim.SetMobilityPollInterval(Seconds(1));
+     
+     std::string fileNameWithNoExtension = "FlowVSThroughput_";
+     std::string graphicsFileName        = fileNameWithNoExtension + ".png";
+     std::string plotFileName            = fileNameWithNoExtension + ".plt";
+     std::string plotTitle               = "Flow vs Throughput";
+     std::string dataTitle               = "Throughput";
+
+     // Instantiate the plot and set its title.
+     Gnuplot gnuplot (graphicsFileName);
+     gnuplot.SetTitle (plotTitle);
+
+     // Make the graphics file, which the plot file will be when it is used with Gnuplot, be a PNG file.
+     gnuplot.SetTerminal ("png");
+
+     // Set the labels for each axis.
+     gnuplot.SetLegend ("Flow", "Throughput");
+
+     Gnuplot2dDataset dataset;
+     dataset.SetTitle (dataTitle);
+     dataset.SetStyle (Gnuplot2dDataset::LINES_POINTS);
+
+    //flowMonitor declaration
+    FlowMonitorHelper fmHelper;
+    Ptr<FlowMonitor> allMon = fmHelper.InstallAll();
+    allMon->CheckForLostPackets ();
+    // call the flow monitor function
+    ThroughputMonitor(&fmHelper, allMon, dataset);
+
+    // trace file, for data extraction
+    AsciiTraceHelper ascii;
+    p2ph.EnableAsciiAll (ascii.CreateFileStream ("tracehandover.tr"));
+    MobilityHelper::EnableAscii (ascii.CreateFileStream ("lena-x2-handover.mob"), ueNodes); //correct for a set of nodes
+
+
+    //Gnuplot ...continued
+    gnuplot.AddDataset (dataset);
+    // Open the plot file.
+    std::ofstream plotFile (plotFileName.c_str());
+    // Write the plot file.
+    gnuplot.GenerateOutput (plotFile);
+    // Close the plot file.
+    plotFile.close ();
+  
   Simulator::Run ();
   Simulator::Destroy ();
   return 0;

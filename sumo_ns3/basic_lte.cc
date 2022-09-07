c2195fb254c3468e31239fcc37a7908ba568608e
Jeet Ahuja <91070824+JeetAhuja15@users.noreply.github.com>
2022-09-08 01:54:38 +0530

    Communication between UE and enB done...

--- sumo_ns3/basic_lte.cc
+++ sumo_ns3/basic_lte.cc
@@ -46,31 +46,35 @@ int main (int argc, char *argv[])
   ipv4h.SetBase ("1.0.0.0", "255.0.0.0");  
   Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign (internetDevices);
   // interface 0 is localhost, 1 is the p2p device
-  Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
+//Ipv4Address remoteHostAddr = internetIpIfaces.GetAddress (1);
 
   // Routing of Internet Host 
   Ipv4StaticRoutingHelper ipv4RoutingHelper;
   Ptr<Ipv4StaticRouting> remoteHostStaticRouting;
   remoteHostStaticRouting = ipv4RoutingHelper.GetStaticRouting (remoteHost->GetObject<Ipv4> ());
-  remoteHostStaticRouting->AddNetworkRouteTo (epcHelper->GetEpcIpv4NetworkAddress (),Ipv4Mask ("255.255.0.0"), 1);
-//remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"),1);
+//remoteHostStaticRouting->AddNetworkRouteTo (epcHelper->GetEpcIpv4NetworkAddress (),Ipv4Mask ("255.255.0.0"), 1);
+  remoteHostStaticRouting->AddNetworkRouteTo (Ipv4Address ("7.0.0.0"), Ipv4Mask ("255.0.0.0"),1);
 
 
   // Creating UEs and EnBs
   NodeContainer ueNodes;
   NodeContainer enbNodes;
-  enbNodes.create (numofEnbs);
-  ueNodes.create (numofUes);
+  enbNodes.Create (numofEnbs);
+  ueNodes.Create (numofUes);
 
-  // Installing Mobility for the EnBs        
+  // Installing Mobility for the EnBs
+  Ptr<ListPositionAllocator> positionAlloc = CreateObject<ListPositionAllocator> ();        
   for (uint16_t i = 0; i<numofEnbs; i++)
   {
-     // X,Y co-ordinates for real-life LTE Towers --> (273.890279027517, 127.135688700248) (124.842975830659, 125.632829552051)
+     // X,Y co-ordinates for real-life LTE Towers --> (273.890279027517, 127.135688700248) (124.842975830659, 125.632829552051) (1005.28760875657, 1502.71367422282) (1046.76811661676, 1611.70460214559)
      if (i==0){
-       positionAlloc->Add (Vector ((273.890279027517, 127.135688700248, 0)));
+       //positionAlloc->Add (Vector (1005.28760875657, 1502.71367422282, 0));
+	positionAlloc->Add (Vector (100, 0 ,0));
      }
      else {
-       positionAlloc->Add (Vector ((124.842975830659, 125.632829552051, 0)));
+       //int j=0;
+	positionAlloc->Add (Vector (0, 100,0));
+//       positionAlloc->Add (Vector  (1046.76811661676, 1611.70460214559, 0));
      }
   }
 
@@ -81,7 +85,11 @@ int main (int argc, char *argv[])
   
 
   // Installing Mobility for UEs
-  
+  /*Ptr<ListPositionAllocator> uepositionAlloc = CreateObject<ListPositionAllocator> ();
+  for (uint16_t i = 0; i<numofUes; i++)
+  { 
+    positionAlloc->Add (Vector (1424.22, 1658.15, 0));
+  }*/
   mobility.SetPositionAllocator ("ns3::GridPositionAllocator",
                                  "MinX", DoubleValue (-50.0),
                                  "MinY", DoubleValue (50.0),
@@ -89,7 +97,9 @@ int main (int argc, char *argv[])
                                  "DeltaY", DoubleValue (10.0),
                                  "GridWidth", UintegerValue (5),
                                  "LayoutType", StringValue ("RowFirst"));
+  
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+//  mobility.SetPositionAllocator (positionAlloc);
   mobility.Install (ueNodes);
   
 
@@ -107,11 +117,33 @@ int main (int argc, char *argv[])
   lteHelper->AttachToClosestEnb (ueLteDevs, enbLteDevs);
 
   // Set default gateway for the UE
-  for (uint32_t = 0; u < numofUes; ++u){
+  for (uint32_t u = 0; u < numofUes; ++u){
     Ptr<Node> ue = ueNodes.Get (u);
     Ptr<Ipv4StaticRouting> ueStaticRouting;
     ueStaticRouting = ipv4RoutingHelper.GetStaticRouting (ue->GetObject<Ipv4> ());
     ueStaticRouting->SetDefaultRoute (epcHelper->GetUeDefaultGatewayAddress (), 1);
-    
+    Ptr<EpcTft> tft = Create<EpcTft> ();
+    EpcTft::PacketFilter pf;
+    pf.localPortStart = 1234;
+    pf.localPortEnd = 1234;
+    tft->Add (pf);
+    lteHelper->ActivateDedicatedEpsBearer (ueLteDevs,EpsBearer (EpsBearer::NGBR_VIDEO_TCP_DEFAULT),tft);
+  
+    uint16_t dlPort = 1234;
+    PacketSinkHelper packetSinkHelper ("ns3::UdpSocketFactory",InetSocketAddress (Ipv4Address::GetAny (), dlPort));
+    ApplicationContainer serverApps = packetSinkHelper.Install (ue);
+    serverApps.Start (Seconds (0.01));
+    UdpClientHelper client (ueIpIfaces.GetAddress (0), dlPort);
+    ApplicationContainer clientApps = client.Install (remoteHost);
+    clientApps.Start (Seconds (0.01));
   } 
+  
+
+
+  Simulator::Stop (simTime);
+  AnimationInterface anim ("my_lte_basic.xml");
+  //anim.SetMobilityPollInterval(Seconds(1));
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
 }
